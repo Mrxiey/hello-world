710d97d7d4ee2acea5925abd6d9af7a37dd36373
Mrxiey <69391626+Mrxiey@users.noreply.github.com>
2020-08-16 02:26:08 +0800

    Add files via upload

---
+++ test/test/test.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include<math.h>
+#define PI 3.14
+int main()
+{
+	/*int a;
+	int b;
+	int c;
+	int d;
+	printf("请输入长方形的长和宽。\n");
+	scanf_s("%d %d", &a, &b);
+	c = (a + b) * 2;
+	d = a * b;
+	printf("长方形的周长为%d,面积为%d.", c, d);
+	*/
+}

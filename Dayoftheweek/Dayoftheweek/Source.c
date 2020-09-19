0158d86aa84c8e87d822f9bf3961a1aef3676347
Mrxiey <69391626+Mrxiey@users.noreply.github.com>
2020-09-20 00:05:00 +0200

    Add files via upload

---
+++ Dayoftheweek/Dayoftheweek/Source.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+	printf("Please enter a number!\n");
+	scanf_s("%d",&a);
+	switch (a)
+	{
+	case 0:
+		printf("error");
+		break;
+	case 1:
+		printf("Monday");
+		break;
+	case 2:
+		printf("Tuesday");
+		break;
+	case 3:
+		printf("Wednesday");
+		break;
+	case 4:
+		printf("Thursday");
+		break;
+	case 5:
+		printf("Friday");
+		break;
+	case 6:
+		printf("Saturday");
+		break;
+	case 7:
+		printf("Sunday");
+		
+		break;
+	default:
+		printf("");
+	}
+	return 0;
+}
